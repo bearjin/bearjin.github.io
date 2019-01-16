03da4a84e275227d185817d04ae054b1aba2f0c3
wj.kim <wj.kim@theuber.co.kr>
2019-01-16 15:27:23 +0900

    test post2

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,3 +1,34 @@
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 })
+
+var a = anime({
+	targets:'.hello',
+	translateX: 270,
+	borderRadius:['0','50%'],
+	elasticity:200,
+	easing:'easeInOutQuad',
+	direction:'alternate',
+	loop:true,
+	update: function(){
+		seekEl.value = a.progress;
+	}
+});
+
+$(".restart").click(function(){
+	a.restart();
+});
+
+$(".pause").click(function(){
+	a.pause();
+});
+
+$(".play").click(function(){
+	a.play();
+});
+
+var seekEl = document.querySelector(".progress");
+
+seekEl.oninput = function(){
+	a.seek(a.duration * (seekEl.value / 100));
+};
